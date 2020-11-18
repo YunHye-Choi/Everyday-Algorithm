3f6c47eb1c352050809908f8e643d387c4e89b34
Yunhye-Choi <anffl98@gmail.com>
2020-11-19 08:33:33 +0900

    원리 이해 못함...복습하기

---
+++ src/DP/boj_11053.java
@@ -0,0 +1,27 @@
+package DP;
+import java.util.*;
+// 복습하기....
+// 참고블로그 : https://wootool.tistory.com/96
+public class boj_11053 {
+    public static void main (String[] args)  {
+        Scanner scanner = new Scanner (System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int [n+1];
+        int dp[] = new int [n+1];
+        for (int i = 1; i <= n; i ++) {
+            arr[i] = scanner.nextInt();
+        }
+        int dp_max = 0;
+        for (int i = 1; i <= n; i ++) {
+            int min = 0;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    min = min < dp[j] ? dp[j] : min;
+                }
+            }
+            dp[i] = min + 1;
+            dp_max = dp_max < dp[i] ? dp[i] : dp_max;
+        }
+        System.out.println(dp_max);
+    }
+}
