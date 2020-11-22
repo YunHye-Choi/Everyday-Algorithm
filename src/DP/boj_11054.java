1b43959205fdaa129ab7f592b085863d4a3ec583
Yunhye-Choi <anffl98@gmail.com>
2020-11-23 08:40:22 +0900

    11054 바이토닉 아직.. 푸는중 .. .

--- src/DP/boj_11054.java
+++ src/DP/boj_11054.java
@@ -2,63 +2,46 @@ package DP;
 import java.util.Scanner;
 
 // 문제: https://www.acmicpc.net/problem/11054
-// 미해결... 풀리정리 아이패드 연습장
+// 해결중
+// 풀이: https://ldgeao99.tistory.com/354
 
 public class boj_11054 {
     public static void main (String[] args) {
         Scanner scanner = new Scanner (System.in);
         int n = scanner.nextInt();
         int arr[] = new int [n];
-        int dp[] = new int [n];
+        int dp_inc[] = new int [n];
+        int dp_dec[] = new int [n];
+        int dp[] = new int[n];
         for (int i = 0; i < n; i ++) {
             arr[i] = scanner.nextInt();
         }
-
-        int max = 0;
-        int max_index = 0;
-        for (int i = 0; i<n;i++){
-            if(max < arr[i]) {
-                max = arr[i];
-                max_index = i;
-            }
-        }
-        dp[0] = 1;
-        boolean peek = false;
+        dp_inc[0] = 1; dp_dec[0] = 1;
         for (int i = 1; i < n; i++) {
-            dp[i] = 1;
-            for (int j = 0;j < i ; j++) {
-                if (peek) { // peek 찍었으니 감소 부분수열 찾자!
-                    if (arr[i] > arr[j] && dp[i] <= dp[j]) {
-                        dp[i] =  dp[j] + 1;
-                    }
-                } else { // peek 아직 안찍었다. 증가 부분수열 구하자
-                    if (arr[i] < arr[j] && dp[i] <= dp[j]) {
-                        dp[i] = dp[j] + 1;
-                    }
+            dp_inc[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] > arr[j] && dp_inc[i] <= dp_inc[j]) {
+                    dp_inc[i] = dp_inc[j] +1;
                 }
             }
-            if(arr[i] == max) {
-                peek = true;
-                if (i != max_index) {
-                    dp[i] = 1;
-                    for (int j = 0;j < i;j++) {
-                        if (arr[i] > arr[j] && dp[i] <= dp[j]) {
-                            dp[i] =  dp[j] + 1;
-                        }
-                    }
-                    if (dp[max_index] < dp[i]) {
-                        max_index = i;
-                    }
-                }
-            }
-
         }
-        int dp_max1 = 0, dp_max2 = 0;
-        for (int i = 0; i<max_index;i++)
-            dp_max1 = Math.max(dp_max1, dp[i]);
-        for (int i = max_index; i<n;i++)
-            dp_max2 = Math.max(dp_max2, dp[i]);
-        int result = dp_max1 + dp_max2;
-        System.out.println(dp_max1 +"+"+ dp_max2 +"="+  result);
+        ///////////단순 감소가 아니라 뒤에서 부터 증가로 생각해야함!!!!!!!!!!
+        for (int i = 1; i < n; i++) {
+            dp_dec[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[i] < arr[j] && dp_dec[i] <= dp_dec[j]) {
+                    dp_dec[i] = dp_dec[j] +1;
+                }
+            }
+        }
+        int dp_max = 0;
+        for (int i = 0; i < n;i ++) {
+            dp[i] = dp_inc[i] + dp_dec[i];
+            System.out.println(dp_inc[i]+" "+dp_dec[i]);
+            dp_max = Math.max(dp[i], dp_max);
+        }
+        int result = dp_max -1;
+        System.out.println(result);
+
     }
 }
