7f1c2178799f7c0e255f3dfdf8f6504dad72e011
Yunhye-Choi <anffl98@gmail.com>
2020-11-24 08:17:43 +0900

    1912끝, 2579해결중, 11054건들인거 없음

---
+++ src/DP/boj_2579.java
@@ -0,0 +1,19 @@
+package DP;
+import java.util.Scanner;
+
+// 문제: https://www.acmicpc.net/problem/2579
+// 푸는 중
+
+public class boj_2579 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int [n];
+        int dp[] = new int [n];
+        for (int i=0;i<n;i++) {
+            arr[i] = scanner.nextInt();
+        }
+        // 규칙찾는중,, 아이패드에 ..
+    }
+
+}
