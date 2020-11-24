5758e044d5a79b0c919c40e8af1deb9835dd2c2c
Yunhye-Choi <anffl98@gmail.com>
2020-11-25 08:44:05 +0900

    1699 시작

---
+++ src/DP/boj_1699.java
@@ -0,0 +1,12 @@
+package DP;
+import java.util.Scanner;
+public class boj_1699 {
+    public static void main (String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int dp[] = new int [n];
+        for(int i=0;i<n;i++) {
+            // 나중에 깊게 생각해보자.
+        }
+    }
+}
