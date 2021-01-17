2a52c3b195d94a4dcb00f97ed4f519ec69fec9c0
Yunhye-Choi <anffl98@gmail.com>
2021-01-18 08:22:25 +0900

    11651 Collections.sort() 이용하여 해결 완료

--- src/Basics/boj_11651_ver2.java
+++ src/Basics/boj_11651_ver2.java
@@ -1,33 +1,58 @@
 package Basics;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator; // 람다식 사용시 필요 없음
 import java.util.Scanner;
 
 // 문제: https://www.acmicpc.net/problem/11651
-// 풀이: https://st-lab.tistory.com/110
-// ver2는 블로그 참고해서 풀기!
+// 풀이: Collections.sort()를 이용하여 풀이
 public class boj_11651_ver2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        int [][] arr = new int[n][2];
+        ArrayList<Coordi> arr = new ArrayList<>();
 
         for (int i = 0; i <n; i++) {
-            arr[i][0] = scanner.nextInt();
-            arr[i][1] = scanner.nextInt();
+            Coordi c = new Coordi(scanner.nextInt(), scanner.nextInt());
+            arr.add(c);
         }
-        // 람다식 이용 (자세한 것은 블로그)
-        Arrays.sort(arr, (e1, e2)-> {
-            if (e1[1] == e2[1]) {
-                return e1[0] - e2[0];
-            } else {
-                return e1[1] - e2[1];
+
+        Collections.sort(arr, new Comparator<Coordi>() {
+            @Override
+            public int compare(Coordi c1, Coordi c2) {
+                if (c1.y == c2.y)
+                    return c1.x - c2.x;
+                else
+                    return c1.y - c2.y;
             }
         });
+        /*
+        위 코드 람다식으로 표현
+        Collections.sort(arr, (c1, c2) -> {
+            if (c1.y == c2.y)
+                return c1.x - c2.x;
+            else
+                return c1.y - c2.y;
+        });
+        * */
 
         for (int i = 0; i < n; i++) {
-            System.out.println(arr[i][0] + " " + arr[i][1]);
+            System.out.println(arr.get(i));
+        }
+    }
+    static class Coordi {
+        int x;
+        int y;
+
+        public Coordi (int _x,int _y) {
+            x = _x;
+            y = _y;
+        }
+        @Override
+        public String toString() {
+            return x + " " + y;
         }
     }
 }
