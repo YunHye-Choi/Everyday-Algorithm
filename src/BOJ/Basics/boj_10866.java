6b8a492b18aa55cc17a3c376730bcca45da2d3ce
Yunhye-Choi <anffl98@gmail.com>
2021-04-27 13:03:49 +0900

    프로그래머스 풀이 추가

--- src/BOJ/Basics/boj_10866.java
+++ src/BOJ/Basics/boj_10866.java
@@ -8,43 +8,59 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class boj_10866 {
-    public static void main (String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String[] commands = new String[n];
-        for (int i = 0; i< n; i ++) {
+        for (int i = 0; i < n; i++) {
             commands[i] = br.readLine();
         }
         Deque deque = new Deque();
         //
 
     }
+
     static class Deque {
         ArrayList<Integer> deques = new ArrayList<>();
-        void push_front (int x) {
 
-        }
-        void push_back (int x) {
-
-        }
-        void pop_front () {
-
-        }
-        void pop_back () {
+        void push_front(int x) {
 
         }
 
-        void size () {
+        void push_back(int x) {
 
         }
-        void empty () {
+
+        void pop_front() {
 
         }
-        void front () {
+
+        void pop_back() {
 
         }
+
+        void size() {
+
+        }
+
+        void empty() {
+
+        }
+
+        void front() {
+
+        }
+
         void back() {
 
         }
     }
+
+
+
+
 }
+
+
+
+
