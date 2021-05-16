ac53d2d38bdecb76f52240e46a55902040823727
Yunhye-Choi <anffl98@gmail.com>
2021-05-16 13:38:52 +0900

    성적이 낮은 순으로 정렬 코멘트 추가

--- src/ThisIsCodingTest/Sort/성적이낮은순서로학생출력하기.java
+++ src/ThisIsCodingTest/Sort/성적이낮은순서로학생출력하기.java
@@ -4,6 +4,9 @@
 //      return e1.value - e2.value; //오름차순!
 //    }
 // }
+// 이 문제에서 해시맵을 쓰면 동명이인을 처리할 수 없다 (이름이 key값이라서)
+// 차라리 ArrayList<ArrayList<String>> 이차원 배열을 선언해서
+// 이름과 점수를 모두 String으로 저장하고 비교시에만 Integer.parseInt()를 사용하자!
 
 import java.util.*;
 public class 성적이낮은순서로학생출력하기 {
