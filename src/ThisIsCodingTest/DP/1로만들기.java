f7a172de45e1c622fbd2c839d59c1f4e7cf84ce2
Yunhye-Choi <anffl98@gmail.com>
2021-06-25 18:13:16 +0900

    1로만들기복습

--- src/ThisIsCodingTest/DP/1로만들기.java
+++ src/ThisIsCodingTest/DP/1로만들기.java
@@ -7,7 +7,9 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-	public static int []dp = new int [30000];
+	// d: 앞서 계산한 결과 산출되는 최소 계산횟수를 저장
+	public static int []d = new int [30001];
+
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// a: X가 5로 나누어떨어지면 5로 나눈다
@@ -17,31 +19,25 @@ class Ideone
 		
 		Scanner sc  = new Scanner (System.in);
 		int x = sc.nextInt();
-		
-		// -1로 초기화
-		for (int i = 0; i < x+1; i ++)
-			dp[i] = -1;
-			
-		dp[1] = 0; // 초항은 0 (x가 1이면 답은 0)
-		System.out.println(solve(x));
-		
-	}
-	
-	public static int solve(int x) {
-		int ans = 0;
+		// d[1] = 0이고 기본적으로 0으로 초기화 되기 때문에 처리할 필요없음
+		// 다이나믹 프로그래밍 진행 (Bottom-Up)
 		for (int i = 2; i <= x; i++) {
-			int min = 40000;
-			if (x % 5 ==0) {
-				min = Math.min(min(dp[x/5], min));
-			} else if (x % 3 == 0) {
-				min = Math.min(min(dp[x/3], min));
-			} else if (x % 2 == 0) {
-				min = Math.min(dp[x/2], min);
-			} else {
-				min = Math.min(dp[x-1], min);
-			}
-			ans ++;
+			// d조건 (연산을 했으므로 +1)
+			d[i] = d[i-1] + 1;
+
+			// c조건 (/2한 연산횟수(d)가 기존 것 보다 작으면 새로 /2라는 연산을 해야하므로 +1)
+			if (i%2 == 0)
+				d[i] = Math.min(d[i], d[i/2] + 1);
+
+			// b조건
+			if (i%3 == 0)
+				d[i] = Math.min(d[i], d[i/3] + 1);
+
+			// a조건
+			if (i%5 == 0)
+				d[i] = Math.min(d[i], d[i/5] + 1);
 		}
-		return ans;
+
+		System.out.println(d[x]);
 	}
 }
