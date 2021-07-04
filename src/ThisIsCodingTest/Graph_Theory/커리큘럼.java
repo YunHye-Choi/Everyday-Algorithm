3b102a859226da2de3c491b0ab73ee97c2e45556
yunhye-choi <anffl98@gmail.com>
2021-07-04 11:11:18 +0900

    커리큘럼 정답코드! 이해 완료

--- src/ThisIsCodingTest/Graph_Theory/커리큘럼.java
+++ src/ThisIsCodingTest/Graph_Theory/커리큘럼.java
@@ -1,68 +1,80 @@
-import java.util.*;
+// topology sort 바로 적용!
+// 문제에서 말하는 최소시간은 !!
+// https://github.com/ndb796/python-for-coding-test/issues/17
+// 위와 같이 두 선수과목을 동시에 듣는다고 했을 때, 
+// 선수과목이 두개라면 두개를 모두 들어야 한다는 가정이므로 max를 사용하여 값을 구하는 것임
 
-class Vertex {
-	private int idx;
-	private int cost;
-	private ArrayList<Integer> inVertex;
-	public Vertex(int i, int c){
-		idx = i;
-		cost = c;
-		inVertex = new ArrayList<Integer>();
-	}
-	public getIdx() {
-		return idx;
-	}
-	public getCost() {
-		return Cost;
-	}
-	public getInVertex(){
-		return inVertex;
-	}
-}
+// 문제이해를 잘하자~~~~
+
+import java.util.*;
+import java.lang.*;
+
 public class Main{
 	public static int v;
-	public static ArrayList<Vertex> vertexes = new ArrayList();
+	public static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 	public static int[] indegree = new int [100001];
-	//public static int[] did = new int [100001];
-	public static int[] ans = new int [100001];
+	public static int[] times = new int [100001];
 	
+	public static void topologySort(){
+		// 기존 result는 add로 적용되나, 이번 경우는 순서대로 출력해야 하므로
+		// c타입 배열을 사용하여 순서가 뒤섞이는 것을 막음
+		int result[] = new int [501];
+		
+		// 처음에는 각 vertex의 시간으로만 초기화
+		for (int i = 0; i <= v; i ++) {
+			result[i] = times[i];
+		}
+
+		// indegree가 0인 vertex를 q에 삽입
+		Queue<Integer> q = new LinkedList<>();
+		for (int i = 1; i <= v; i ++) {
+			if (indegree[i] == 0)
+				q.offer(i);
+		}
+
+		while(!q.isEmpty()) {
+			// 큐에서 원소 꺼내기
+			int now = q.poll();
+
+			for (int i : graph.get(now)) {
+				// 
+				result[i] = Math.max(result[i], result[now] + times[i]);
+				// 해당 원소와 연결된 노드들의 진입차수에서 1을 빼기
+				indegree[i]--;
+
+				// 새롭게 진입차수가 0이 된 vertex를 큐에 삽입
+				if (indegree[i] == 0)
+					q.offer(i);
+			}
+		}
+
+		for(int i = 1; i <= v; i ++) {
+			System.out.println(result[i]);
+		}
+	}
+
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		v = sc.nextInt();
 		
-		vertexes.add(new Vertex(0, 0));
-		
+		for (int i = 0; i <= v; i ++) {
+			graph.add(new ArrayList<Integer>());
+		}
+
 		for (int i = 1; i <= v; i++) {
-			int c = sc.nextInt();
-			vertexes.add(new Vertex(i, c));
-			int num = sc.nextInt();
-			while(num != -1) {
-				vertexes.get(i).getInVertex().add(new Vertex());
+			int x = sc.nextInt();
+			times[i] = x;
+			x = sc.nextInt();
+			while(x != -1) {
+				indegree[i] ++;
+				graph.get(i).add(i);
+				x = sc.nextInt();
 			}
 		}
 		
-		for (int i = 1; i <= vertexes.size(); i ++) {
-			Vertex v = vertexes.get(i)
-			ans[i] += v.getCost();
-			while (v.getInVertex().size() != 0) {
-				int minCost = 1000001;
-				int minIdx = -1;
-				for (int verIdx : v.getInvertex()){
-					if (vertexes.get(verIdx).getCost() < minCost) {
-						minCost = vertexes.get(verIdx).getCost();
-						minIdx = verIdx;
-					}
-				}
-				ans[i] += minCost;
-				v = vertexes.get(minIdx);
-			}
-		}
-		
-		for (int i = 1; i <= v; i++) {
-			System.out.println(ans[i]);
-		}
+		topologySort();
 	}
 	
 }
