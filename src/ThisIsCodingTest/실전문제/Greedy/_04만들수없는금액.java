711ce1d3663e396cb407e48eefaf13ceb1434c34
Yunhye-Choi <anffl98@gmail.com>
2021-07-12 00:28:05 +0900

    hello

--- src/ThisIsCodingTest/실전문제/Greedy/_04만들수없는금액.java
+++ src/ThisIsCodingTest/실전문제/Greedy/_04만들수없는금액.java
@@ -10,18 +10,30 @@ public class Main {
 		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		int num = 1;
-	
+		
 		Array.sort(arr);
 		
-		while(true) {
+		/*while(true) {
 			// 로직 구현해야 함
 			// 아이디어:
 			// num을 1씩 늘려가며 반복문 수행
 			num++
 			
 			// 만약 arr들의 합으로 num이 구해지면 continue;
+			// 수들의 조합의 모든 경우의 수를 따져봐야 할듯
+			// 방법을 모르겟다.
 			// else, break
+			
+			
+		}*/
+		// 이하 정답코드
+		int num = 1;
+		for (int i = 0; i < n; i ++) {
+			//만들 수 없는 금액을 찾았을 때 반복 종료
+			if (num < arr[i]) break;
+			num += arr[i];
 		}
+		
+		System.out.println(num);
 	}
 }
