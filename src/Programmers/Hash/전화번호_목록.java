3cc26dc49067b9f80c22bd4c522dd003a9aebf71
Yunhye-Choi <anffl98@gmail.com>
2021-10-26 21:10:22 +0900

    전화번호 목록 해시맵 풀이 리뷰

--- src/Programmers/Hash/전화번호_목록.java
+++ src/Programmers/Hash/전화번호_목록.java
@@ -28,30 +28,27 @@
 //     }
 // }
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    public boolean solution(String[] phone_book){
+    public boolean solution(String [] phone_book) {
         boolean answer = true;
         
-        // 1. HashMap 만들기
-        HashMap<String, Integer> map = new HashMap<>();
+        // hash map 만들기
+        HashMap<String,Integer> hm = new HashMap<>();
         
-        // 2. 모든 전화번호로 hashing(==해시맵에추가)하기
-        for(int i = 0; i < phone_book.length; i++) {
-            map.put(phone_book[i],i);
+        // 모든 전화번호를 접두어의 후보에 포함
+        for (int i = 0; i < phone_book.length; i++) {
+            hm.put(phone_book[i], i);
         }
         
-        // 3. 다시 돌며 각 전화번호의 일부가 해시맵에 있는지 탐색
-        // phone_book[i]가 '포함하는', 해쉬셋의 값이 '포함되는' 값이라고 생각하기
-        for(int i = 0; i < phone_book.length; i ++) {
-            for (int j = 1; j < phone_book[i].length(); j++) {
-                if (map.containsKey(phone_book[i].substring(0,j))) {
-                    return false;
-                }
+        // 해시맵에 저장된 접두어를 꺼내보며 전화번호의 앞부분과 일치하는지 비교
+        for (int i = 0; i < phone_book.length; i++) {
+            for(int j = 1; j < phone_book[i].length(); j++) {
+                if (hm.containsKey(phone_book[i].substring(0, j))) return false;
             }
         }
-        return true;
+        
+        return answer;
     }
 }
