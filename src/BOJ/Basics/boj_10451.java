a8240829c3dc80ae26edb449f34cd673fc463d82
Yunhye-Choi <anffl98@gmail.com>
2022-01-06 20:00:18 +0900

    쉽게풀었음 - dfs이용

---
+++ src/BOJ/Basics/boj_10451.java
@@ -0,0 +1,45 @@
+// https://www.acmicpc.net/problem/10451
+
+import java.util.*;
+public class boj_10451 {
+    public static ArrayList<Integer> [] graph;
+    public static boolean visit[];
+    public static int ans;
+    public static void main(String[]args) {
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        while(t-- != 0) {
+            int n = sc.nextInt();
+            graph = new ArrayList[n+1];
+            visit = new boolean[n+1];
+            ans = 0;
+            for (int i = 1; i <= n ; i ++) {
+                graph[i] = new ArrayList<>();
+            }
+            for (int i = 1; i <=n ; i++) {
+                int end = sc.nextInt();
+                graph[i].add(end);
+            }
+
+            for(int i = 1; i <= n; i++) {
+                if(!visit[i]) {
+                    dfs(i,i);
+                }
+            }
+
+            System.out.println(ans);
+
+        }
+    }
+    public static void dfs(int n, int first) {
+        visit[n] = true;
+        for(int i: graph[n]) {
+            if (!visit[i]) {
+                dfs(i, first);
+            } else if (i == first) {
+                ans ++;
+                return;
+            }
+        }
+    }
+}
