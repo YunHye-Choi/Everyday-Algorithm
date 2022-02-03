360ff51023bee1cc8e4dc0178ae9dfd1510765ce
Yunhye-Choi <anffl98@gmail.com>
2022-02-04 07:04:12 +0900

    11057 해곃

--- src/BOJ/DP/boj_11057.java
+++ src/BOJ/DP/boj_11057.java
@@ -9,16 +9,32 @@ public class boj_11057 {
 
         int [][] dp = new int[n+1][10];
         
-        for (int i = 1; i <= 9; i++) {
+        for (int i = 0; i <= 9; i++) {
             dp[1][i] = 1;
         }
 
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j <= 9; j++) {
-                for (int k = j; k<= 9; k++) {
+        if (n >= 2) {
+            for (int i = 0; i <= 9; i++) {
+                dp[2][i] = i+1;
+            }
+        }
 
+        if (n >= 3) {
+            for (int i = 3; i <= n; i++) { // 길이에 따라 2~n
+                for (int j = 0; j <= 9; j++) { // 
+                    for (int k = 0; k<= j; k++) {
+                        dp[i][j] += dp[i-1][k];
+                    }
+                    dp[i][j] %= mod;
                 }
             }
         }
+        
+
+        int ans  = 0;
+        for(int i = 0; i <= 9; i ++) {
+            ans += dp[n][i];
+        }
+        System.out.println(ans % mod);
     }
 }
