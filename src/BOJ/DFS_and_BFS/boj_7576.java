05ea50e2eaf4294eb1889c42f235c2be86357b18
Yunhye-Choi <anffl98@gmail.com>
2022-02-15 07:42:24 +0900

    bfs 이용하여 해결 중

--- src/BOJ/DFS_and_BFS/boj_7576.java
+++ src/BOJ/DFS_and_BFS/boj_7576.java
@@ -1,93 +1,52 @@
 // https://www.acmicpc.net/problem/7576
 import java.util.*;
-import java.io.*;
- 
-// https://www.acmicpc.net/problem/7576
- 
-class Main {
-    static int N;
-    static int M;
-    static int[][] box;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
- 
-    static class Dot {
-        int x;
-        int y;
-        int day;
- 
-        public Dot(int x, int y, int day) {
-            this.x = x;
-            this.y = y;
-            this.day = day;
-        }
-    }
- 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
- 
-        box = new int[M][N];
- 
-        for(int i=0; i<M; i++) {
-            st = new StringTokenizer(br.readLine());
- 
-            for(int j=0; j<N; j++) {
-                box[i][j] = Integer.parseInt(st.nextToken());
+public class boj_7576 {
+    public static int m, n;
+    public static int [][] map;
+    public static boolean [][] visit;
+    public static int [] dx = {0, 0, -1, 1};
+    public static int [] dy = {1, -1, 0, 0};
+    public static int ans;
+    
+    public static void main(String [] args) {
+        Scanner sc = new Scanner (System.in);
+        m = sc.nextInt(); // x
+        n = sc.nextInt(); // y
+        ans = 0;
+        map = new int[n][m];
+        visit = new boolean[n][m];
+
+        for (int i = 0; i < n ; i ++) {
+            for (int j = 0; j < m; j ++) {  
+                map[i][j] = sc.nextInt();
             }
         }
- 
-        bfs();
-    }
- 
-    static void bfs() {
-        Queue<Dot> q = new LinkedList<Dot>();
-        int day = 0;
- 
-        // 토마토가 있는 좌표 찾아서 Queue에 넣기
-        for(int i=0; i<M; i++) {
-            for(int j=0; j<N; j++) {
-                if(box[i][j] == 1) 
-                    q.offer(new Dot(i, j, 0));
-            }
-        }
- 
-        // bfs 시작
-        while(!q.isEmpty()) {
-            Dot dot = q.poll();
-            day = dot.day;
- 
-            for(int i=0; i<4; i++) {
-                int nx = dot.x + dx[i];
-                int ny = dot.y + dy[i];
- 
-                if(0 <= nx && nx < M && 0 <= ny && ny < N) {
-                    if(box[nx][ny] == 0) {
-                        box[nx][ny] = 1;
-                        q.add(new Dot(nx, ny, day+1));
-                    }
+
+        for (int i = 0; i < n; i ++) { // y
+            for (int j = 0; j < m; j++) { // x
+                if (!visit[i][j] && map[i][j] == 1) {
+                    bfs(j, i);
                 }
             }
         }
- 
-        // 토마토가 다 익었는지 확인
-        if(checkTomato())
-            System.out.println(day);
-        else
-            System.out.println(-1);
-    }    
- 
-    // box 배열에 0이 남아있다면 false, 아니면 true
-    static boolean checkTomato() {
-        for(int i=0; i<M; i++) {
-            for(int j=0; j<N; j++) {
-                if(box[i][j] == 0)
-                    return false;
+    }
+    public static void bfs(int x, int y){
+        visit[y][x] = true;
+
+        Queue<Integer[]> q = new LinkedList<>();
+
+        for (int i = 0; i < 4; i ++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+                if (map[ny][nx] == 0) {
+                    Integer[] temp = {nx, ny};
+                    q.add(temp);
+                    ans ++;
+                }
             }
         }
- 
-        return true;
+        while
     }
 }
