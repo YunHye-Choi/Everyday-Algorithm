3e7b76b1053d9ceb48c2a26241a7c37602a9163e
Yunhye-Choi <anffl98@gmail.com>
2022-04-16 01:02:01 +0900

    코테공부 가보자고

--- src/BOJ/DFS_and_BFS/boj_7576.java
+++ src/BOJ/DFS_and_BFS/boj_7576.java
@@ -1,4 +1,6 @@
 // https://www.acmicpc.net/problem/7576
+// 풀이참고: https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-7576%EB%B2%88-%ED%86%A0%EB%A7%88%ED%86%A0-%EC%9E%90%EB%B0%94Java
+
 import java.util.*;
 
 class Tomato {
