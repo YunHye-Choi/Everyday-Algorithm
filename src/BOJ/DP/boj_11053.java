c88c69f3e075986a98c91b4beed4c4c527d6993d
Yunhye-Choi <anffl98@gmail.com>
2022-05-09 23:17:29 +0900

    해결중~

---
+++ src/BOJ/DP/boj_11053.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/11053
+import java.util.*;
+public class boj_11053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr, dp;
+        
+        arr = new int[n];
+        dp = new int[n];
+
+        for (int i = 0; i < n; i ++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+}
