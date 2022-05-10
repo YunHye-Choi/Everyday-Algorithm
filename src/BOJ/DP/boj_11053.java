c20e2556ba58219e32a4fb3c5de7dcdfa8e6fb79
Yunhye-Choi <anffl98@gmail.com>
2022-05-10 22:36:48 +0900

    가장 긴 부분수열 해결!

--- src/BOJ/DP/boj_11053.java
+++ src/BOJ/DP/boj_11053.java
@@ -1,4 +1,5 @@
 // https://www.acmicpc.net/problem/11053
+// https://developer-mac.tistory.com/71 를 참고했으나 불필요한 조건 제거하여 해결
 import java.util.*;
 public class boj_11053 {
     public static void main(String[] args) {
@@ -12,5 +13,19 @@ public class boj_11053 {
         for (int i = 0; i < n; i ++) {
             arr[i] = sc.nextInt();
         }
+
+        dp[0] = 1;
+        int max = 1;
+
+        for (int i = 1; i < n; i ++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j ++) {
+                if (arr[i] > arr[j] && dp[i] == dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            max = max < dp[i] ? dp[i] : max;
+        }
+        System.out.println(max);
     }
 }
