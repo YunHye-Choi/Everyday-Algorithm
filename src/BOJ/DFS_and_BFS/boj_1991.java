e9f3caca35e0e5f9ff27d1d93f8142e4bb81e982
Yunhye-Choi <anffl98@gmail.com>
2022-05-12 20:47:16 +0900

    1991 복습완

--- src/BOJ/DFS_and_BFS/boj_1991.java
+++ src/BOJ/DFS_and_BFS/boj_1991.java
@@ -6,27 +6,26 @@ class Node {
     Node left;
     Node right;
 
-    public Node (char v) {
-        this.val = v;
+    public Node(char val) {
+        this.val = val;
     }
 
     public String toString() {
-        return val + "";
+        return val+"";
     }
 }
 public class boj_1991 {
-    public static Node[] tree;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        tree = new Node[n];
-        sc.nextLine();
+        Node[] tree = new Node[n];
         char a = 'A';
-        for(int i = 0; i < n; i ++) {
-            tree[i] = new Node(a++);
+        for (int i = 0; i < n ; i ++) {
+            tree[i] = new Node(a ++);
         }
-
-        for(int i = 0; i < n ; i ++) {
+        sc.nextLine();
+        
+        for (int i = 0; i < n ; i ++ ) {
             char[] arr = sc.nextLine().toCharArray();
             tree[arr[0]-65].left = arr[2] == '.'? null : tree[arr[2] - 65];
             tree[arr[0]-65].right = arr[4] == '.'? null : tree[arr[4] - 65];
@@ -35,38 +34,27 @@ public class boj_1991 {
         System.out.println(preorder(tree[0]));
         System.out.println(inorder(tree[0]));
         System.out.println(postorder(tree[0]));
+
     }
-    private static String preorder(Node n) {
-        String retVal =  "";
-        retVal += n.val;
-        if(n.left != null) {
-            retVal += preorder(n.left);
-        }
-        if(n.right != null) {
-            retVal += preorder(n.right);
-        }
-        return retVal;
+    private static String preorder(Node node) {
+        String ans = "";
+        ans += node.val;
+        if (node.left != null) ans += preorder(node.left);
+        if (node.right != null) ans += preorder(node.right);
+        return ans;
     }
-    private static String inorder(Node n) {
-        String retVal =  "";
-        if(n.left != null) {
-            retVal += inorder(n.left);
-        }
-        retVal += n.val;
-        if(n.right != null) {
-            retVal += inorder(n.right);
-        }
-        return retVal;
+    private static String inorder(Node node) {
+        String ans = "";
+        if (node.left != null) ans += inorder(node.left);
+        ans += node.val;
+        if (node.right != null) ans += inorder(node.right);
+        return ans;
     }
-    private static String postorder(Node n) {
-        String retVal =  "";
-        if(n.left != null) {
-            retVal += postorder(n.left);
-        }
-        if(n.right != null) {
-            retVal += postorder(n.right);
-        }
-        retVal += n.val;
-        return retVal;
+    private static String postorder(Node node) {
+        String ans = "";
+        if (node.left != null) ans += postorder(node.left);
+        if (node.right != null) ans += postorder(node.right);
+        ans += node.val;
+        return ans;
     }
 }
