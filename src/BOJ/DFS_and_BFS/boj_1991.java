480894247941696d3d879a09b250b3bdf403f64e
Yunhye-Choi <anffl98@gmail.com>
2022-05-12 17:47:51 +0900

    1991 풀이 참조하여 해결 완, 복습필요!!

--- src/BOJ/DFS_and_BFS/boj_1991.java
+++ src/BOJ/DFS_and_BFS/boj_1991.java
@@ -1,4 +1,5 @@
 // https://www.acmicpc.net/problem/1991
+// 풀이: https://codingjerk-diary.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%ED%92%80%EC%9D%B4%EB%B0%B1%EC%A4%80-1991-%ED%8A%B8%EB%A6%AC-%EC%88%9C%ED%9A%8C-JAVA
 import java.util.*;
 class Node {
     char val;
