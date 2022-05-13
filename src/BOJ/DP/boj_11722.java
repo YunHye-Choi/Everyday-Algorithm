2ef27f8d4987974e74ef1b2acee90955dd753a24
Yunhye-Choi <anffl98@gmail.com>
2022-05-14 00:50:59 +0900

    복습요망.. 익숙하단 것에 연연하지 말고 차근하게 생각할 것.

--- src/BOJ/DP/boj_11722.java
+++ src/BOJ/DP/boj_11722.java
@@ -1,3 +1,7 @@
+// https://www.acmicpc.net/problem/11722
+// 비슷한 문제 푼 적 있다고 발 동동구르며 "뭐였지" 할 시간에 
+// 초심으로 돌아가서 차근차근 생각하자... :(
+// 풀이 참고: 11053(가장 긴 증가하는 부분 수열)
 import java.util.*;
 public class boj_11722 {
     public static void main(String[] args) {
@@ -9,16 +13,16 @@ public class boj_11722 {
             arr[i] = sc.nextInt();
         }
         dp[0] = 1;
-        int maxdp = 1;
-        for(int i = 1; i < n; i ++) {
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
             dp[i] = 1;
-            for(int j = 0; j < i; j++) {                
-                if(arr[j] > arr[i] && dp[i] == dp[j]) {
+            for (int j = 0; j < i; j ++) {
+                if (arr[j] > arr[i] && dp[j] == dp[i])
                     dp[i] = dp[j] + 1;
-                }
             }
-            maxdp = maxdp < dp[i] ? dp[i] : maxdp;
+            ans = ans < dp[i] ? dp[i] : ans;
         }
-        System.out.println(maxdp);
+
+        System.out.println(ans);
     }
 }
