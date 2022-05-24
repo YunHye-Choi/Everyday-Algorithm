7329f9ff32036f672a3a31b9db9ae0af607090cb
Yunhye-Choi <anffl98@gmail.com>
2022-05-25 00:00:17 +0900

    차근히 생각중

--- src/BOJ/DFS_and_BFS/boj_11725.java
+++ src/BOJ/DFS_and_BFS/boj_11725.java
@@ -1,4 +1,7 @@
 //  https://www.acmicpc.net/problem/11725
+// 순서 무관하게 두 점을 n-1개 입력받고
+// 2번노드부터 각 노드의 부모 출력.
+// 단, 1번노드는 root
 import java.util.*;
 class Node {
     int num;
@@ -54,13 +57,47 @@ class Tree {
         }
     }
 }
+class Edge {
+    int s; // smaller
+    int l; // larger
+
+    public Edge(int n1, int n2) {
+        if (n1 < n2) {
+            this.s = n1;
+            this.l = n2;
+        } else {
+            this.s = n2;
+            this.l = n1;
+        }
+    }
+}
 public class boj_11725 {
-    Scanner sc = new Scanner (System.in);
-    int n = sc.nextInt();
-    for (int i = 0; i < n-1; i ++) {
-        sc.nextInt();
-        // 순서 무관하게 두 점을 n-1개 입력받고
-        // 2번노드부터 각 노드의 부모 출력.
-        // 단, 1번노드는 root
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        // arrayList edge 생성 후, node1 기준으로 정렬 한 다음
+        // tree에 node 추가
+        List<Edge> edges = new ArrayList<>();
+        for (int i = 0; i < n-1; i ++) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            edges.add(new Edge(n1, n2));
+        }
+        
+        Comparator<Edge> c = new Comparator<Edge>() {
+            @Override
+            public int compare(Edge e1, Edge e2) {
+                return e1.s - e2.s != 0 ? e1.s - e2.s : e1.l - e2.l;
+            }
+        };
+
+        Collections.sort(edges, c);
+        Tree tree = new Tree(new Node(1));
+        for(Edge e : edges) {
+            tree.addNode(new Node(e.s), new Node(e.l));
+        }
+
+        
+
     }
 }
