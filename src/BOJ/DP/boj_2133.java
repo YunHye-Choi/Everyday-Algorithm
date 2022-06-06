ae7e73732bad769453eb369d4c2a0e6607564a54
Yunhye-Choi <anffl98@gmail.com>
2022-06-07 00:50:04 +0900

    dp 푸는중

---
+++ src/BOJ/DP/boj_2133.java
@@ -0,0 +1,9 @@
+// 풀이 참고:https://yabmoons.tistory.com/536 해보기
+// 해결중
+import java.util.*;
+public class boj_2133 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
