9ba0402c17da61c06c114d014236b8a6639cabd4
Yunhye-Choi <anffl98@gmail.com>
2022-06-07 14:21:02 +0900

    dp 풀이 참고하여 해결. 풀이보다 생각 과정을 연습하기

--- src/BOJ/DP/boj_2133.java
+++ src/BOJ/DP/boj_2133.java
@@ -5,5 +5,19 @@ public class boj_2133 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] dp = new int [n+1];
+        if (n >= 2)
+            dp[2] = 3; // 초항
+        if (n % 2 == 0) {
+            for (int i = 4; i <= n; i+=2) {
+                int k = i-2;
+                dp[i] = dp[k] * dp[2] + 2;
+                while(k > 2) {
+                    k -= 2;
+                    dp[i] += dp[k] * 2;
+                }
+            }
+        }
+        System.out.println(dp[n]);
     }
 }
