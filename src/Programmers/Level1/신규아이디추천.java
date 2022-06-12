dd0cae8cffa9664795ac8f6c7edf15d46d6cd7e5
anffl98@gmail.com <anffl98@gmail.com>
2022-06-12 09:06:07 +0900

    정규식 사용법 잘 메모해두고 활용하자

--- src/Programmers/Level1/신규아이디추천.java
+++ src/Programmers/Level1/신규아이디추천.java
@@ -1,74 +1,25 @@
-// 문자열 메서드 복습 용 문제. 정규형을 이용한 풀이까지 익혀보자!
-// 일단 내 코드 잘못된점 찾기 (테케 2개 통과못함)
-// ㄴ 잘안되면 풀이 참고하기: https://minhamina.tistory.com/121
+// 정규식 사용!!!!
+// https://minhamina.tistory.com/121
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
-        new_id = new_id.toLowerCase();
-        char[] arr = new_id.toCharArray();
+        // 정규식 이용!!
+        String id = new_id.toLowerCase();
+        id = id.replaceAll("[^-_.a-z0-9]", "");
+        id = id.replaceAll("[.]{2,}", ".");
+        id = id.replaceAll("^[.]|[.]$", "");
         
-        for(int i = 0 ; i < arr.length; i++) {
-            if(!('a' <= arr[i] && arr[i]<='z') && !('0' <= arr[i] && arr[i]<='9')
-              && arr[i] != '.' && arr[i] != '-' && arr[i] != '_') {
-                String target = Character.toString(arr[i]);
-                new_id = new_id.replace(target, "");
-            }
+        if (id.equals("")){
+            id = "a";
         }
         
-        arr = new_id.toCharArray();
-        String dots = ".";
-        boolean prevDot = false;
-        for(int i = 1 ; i < arr.length; i++) {
-            if (arr[i-1] == '.' && arr[i] == '.') {
-                System.out.println(arr[i-1] + " vs " + arr[i]);
-                dots += ".";
-                prevDot = true;
-                if (i == arr.length-1) {
-                    new_id = new_id.replace(dots, ".");
-                }
-            } else {
-                if (prevDot) {
-                    new_id = new_id.replace(dots, ".");
-                    dots=".";
-                    prevDot = false;
-                }
-            }
+        if (id.length() >= 16) {
+            id = id.substring(0, 15);
+            id = id.replaceAll("^[.]|[.]$", "");
         }
+        while(id.length() < 3){
+            id += id.charAt(id.length()-1);
+        } 
         
-        arr = new_id.toCharArray();
-        if (arr[0] == '.'){
-            if(arr.length >= 1)
-                new_id = new_id.substring(1);
-        }
-        arr = new_id.toCharArray();
-        if (arr.length >= 1) {
-            if (arr[arr.length-1] == '.'){
-                if(new_id.length() >= 1) {
-                    new_id = new_id.substring(0, new_id.length()-1);
-                }
-                else new_id = "";
-            }
-        }
-        if (new_id.length() == 0) {
-            new_id = "a";
-        }
-        if (new_id.length() > 15) {
-            new_id = new_id.substring(0, 15);
-            
-        }
-        arr = new_id.toCharArray();
-        if (arr[new_id.length()-1] == '.') {
-            new_id = new_id.substring(0, new_id.length()-1);
-        }
-        
-        arr = new_id.toCharArray();
-        if (new_id.length() < 3) {
-            String last = Character.toString(arr[new_id.length()-1]);
-            while (new_id.length() < 3) {
-                new_id += last;
-            }
-        }
-        answer = new_id;
-        return answer;
+        return id;
     }
 }
