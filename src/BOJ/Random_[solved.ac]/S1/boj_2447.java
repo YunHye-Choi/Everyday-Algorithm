fa9e034f92ef63984fc428663328bb3f6ce552fc
anffl98@gmail.com <anffl98@gmail.com>
2022-06-20 13:40:21 +0900

    분할정복 알고리즘.. 떠올리기 어렵다. 내 방식대로 정리해보자

--- src/BOJ/Random_[solved.ac]/S1/boj_2447.java
+++ src/BOJ/Random_[solved.ac]/S1/boj_2447.java
@@ -1,37 +1,49 @@
+// https://www.acmicpc.net/problem/2447
+// 풀이 참고: https://st-lab.tistory.com/95
 import java.util.*;
 public class boj_2447 {
+    public static char [][] arr;
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        char[][] mini = new char[3][3];
-        for (int i = 0; i < 3; i ++){
-            for (int j = 0; j < 3; j++) {
-                if (i == j && i == 1) {
-                    mini[i][j] = ' ';
-                } else {
-                    mini[i][j] = '*';
-                }
-            }
-        }
         int n = sc.nextInt();
-        if (n == 1) {
-            for (int i = 0; i < 3; i ++){
-                for (int j = 0; j < 3; j++) {
-                    System.out.println(Character.toString(mini[i][j]));
+        arr = new char [n][n];
+        star(0, 0, n, false);
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j++){
+                sb.append(arr[i][j]+"");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static void star(int r, int c, int N, boolean blank) {
+        if(blank){
+            for (int i = r; i < r+N; i++) {
+                for (int j = c; j < c+N; j++) {
+                    arr[i][j] = ' ';
                 }
             }
             return;
         }
-        StringBuilder sb = new StringBuilder();
-        int k = (int) Math.cbrt(n); // n == 3^k
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i <= Math.pow(3, k-1) && i < Math.pow(3, k-1) * 2 - 1) {
-                    sb.append(" ");
-                }else if (){
-                    
+        
+        if (N == 1) {
+            arr[r][c] = '*';
+            return;
+        }
+
+        int size = N/3;
+        int count = 0;
+        for (int i = r; i < r+N; i += size) {
+            for (int j = c; j < c+N; j+= size) {
+                if (count == 4){
+                    star(i, j, size, true);
+                } else{
+                    star(i, j, size, false);
                 }
+                count ++;
             }
-            sb.append("\n");
         }
     }
 }
