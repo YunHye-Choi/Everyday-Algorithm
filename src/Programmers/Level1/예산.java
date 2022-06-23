05ff0639d8007ec56247739f94218a408ded99f1
anffl98@gmail.com <anffl98@gmail.com>
2022-06-23 16:53:24 +0900

    할게 많아서 오늘은 여기까지..

---
+++ src/Programmers/Level1/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for (int i : d) {
+            if (budget >= i){
+                budget -= i;
+                answer++;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
