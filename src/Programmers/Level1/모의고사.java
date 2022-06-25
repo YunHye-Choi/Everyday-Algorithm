37f59c9cddf995cf2ded238188a10970830c7017
anffl98@gmail.com <anffl98@gmail.com>
2022-06-26 00:00:56 +0900

    풀었지롱 희희

--- src/Programmers/Level1/모의고사.java
+++ src/Programmers/Level1/모의고사.java
@@ -6,7 +6,7 @@ class Solution {
         int a = 0, b = 0, c = 0;
         // 수포자 a
         for (int i = 0; i < answers.length;i ++) {
-            if (i%5 == 0) {
+            if (i == 0 || i%5 == 0) {
                 if (answers[i] == 1) a++;
             } else if (i%5 == 1) {
                 if (answers[i] == 2) a++;
@@ -22,20 +22,22 @@ class Solution {
         
         // 수포자 b
         for (int i = 0; i < answers.length;i ++) {
-            if (i%2 == 0) {
+            if (i == 0 || i%2 == 0) {
                 if (answers[i] == 2) b++;
-            } else if (i%7 == 1) {
+            } else if (i%8 == 1) {
                 if (answers[i] == 1) b++;
-            } else if (i%7 == 3) {
+            } else if (i%8 == 3) {
                 if (answers[i] == 3) b++;
-            } else if (i%7 == 5) {
+            } else if (i%8 == 5) {
+                if (answers[i] == 4) b++;
+            } else if (i%8 == 7) {
                 if (answers[i] == 5) b++;
             }
         }
         
         // 수포자 c
         for (int i = 0; i < answers.length;i ++) {
-            if (i%10 == 0 || i%10 == 1) {
+            if (i == 0 || i%10 == 0 || i%10 == 1) {
                 if (answers[i] == 3) c++;
             } else if (i%10 == 2 || i%10 == 3) {
                 if (answers[i] == 1) c++;
@@ -48,9 +50,7 @@ class Solution {
             }
         }
         
-        int max = Math.max(a, b);
-        max = Math.max(b, c);
-        max = Math.max(a, c);
+        int max = Math.max(a, Math.max(b, c));
         
         if (a == max) arr.add(1);
         if (b == max) arr.add(2);
@@ -60,10 +60,7 @@ class Solution {
         int[] answer = new int[arr.size()];
         for(int i = 0; i < answer.length; i++){
             answer[i] = arr.get(i);
-            System.out.println(answer[i]);
         }
-
-        
         return answer;
     }
 }
