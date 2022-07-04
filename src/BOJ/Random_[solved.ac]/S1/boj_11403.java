6ad4f98c1f98578ea9b762e6b05574069f7857b3
anffl98@gmail.com <anffl98@gmail.com>
2022-07-04 23:57:01 +0900

    11403 풀이시작

---
+++ src/BOJ/Random_[solved.ac]/S1/boj_11403.java
@@ -0,0 +1,8 @@
+// https://www.acmicpc.net/problem/11403
+import java.util.*;
+public class boj_11403 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
