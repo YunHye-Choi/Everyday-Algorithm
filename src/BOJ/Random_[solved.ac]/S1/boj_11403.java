fb1c713b8d5380d62157bef8a71d592957c51cc9
anffl98@gmail.com <anffl98@gmail.com>
2022-07-08 23:18:10 +0900

    해결중, DFS 사용 예정

--- src/BOJ/Random_[solved.ac]/S1/boj_11403.java
+++ src/BOJ/Random_[solved.ac]/S1/boj_11403.java
@@ -4,6 +4,19 @@ public class boj_11403 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] graph = new int[n][n];
+        int[][] ans = new int[n][n];
+        
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = -1;
+            }
+        }
         
     }
 }
