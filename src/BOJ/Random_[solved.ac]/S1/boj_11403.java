7ce06b7a58427c6da54c1327a7312368a085b851
anffl98@gmail.com <anffl98@gmail.com>
2022-07-15 22:34:49 +0900

    해결중

--- src/BOJ/Random_[solved.ac]/S1/boj_11403.java
+++ src/BOJ/Random_[solved.ac]/S1/boj_11403.java
@@ -1,24 +1,50 @@
 // https://www.acmicpc.net/problem/11403
 import java.util.*;
 public class boj_11403 {
+    public static boolean[] visit;
+    public static int[][] ans;
+    public static ArrayList<Integer>[] graph;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[][] graph = new int[n][n];
-        int[][] ans = new int[n][n];
+        ans = new int[n][n];
+        graph = new ArrayList[n];
         
         for (int i = 0; i < n; i ++) {
+            graph[i] = new ArrayList<>();
             for (int j = 0; j < n; j++) {
-                graph[i][j] = sc.nextInt();
+                int num = sc.nextInt();
+                if (num == 1) {
+                    graph[i].add(j);
+                }
             }
         }
+        
+        // DFS 사용하여 해결하면 됨!
+        // ans를 순회
+        // graph가 1인 경우
+        for (int i = 0; i < n; i ++) {
+            visit = new boolean[n];
+            for (int j = 0; j < n; j++) {
+                if (dfs(i, j)) ans[i][j] = 1;
+            }
+        }
+
         for (int i = 0; i < n; i ++) {
             for (int j = 0; j < n; j++) {
-                ans[i][j] = -1;
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    private static boolean dfs(int start, int end) {
+        visit[start] = true;
+        for (int num: graph[start]){
+            if (num == end) return true;
+            if (!visit[num]) {
+                dfs(num, end);
             }
         }
- 
-        // DFS 사용하여 해결하면 됨!
-
+        return false;
     }
 }
