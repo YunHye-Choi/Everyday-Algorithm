2c55bbd31afa56a662e0135f994da2cd4aea5d0b
anffl98@gmail.com <anffl98@gmail.com>
2022-08-28 22:23:50 +0900

    bfs이용

--- src/BOJ/Random_[solved.ac]/S1/boj_11403.java
+++ src/BOJ/Random_[solved.ac]/S1/boj_11403.java
@@ -1,41 +1,49 @@
-// https://www.acmicpc.net/problem/11403
-// https://c-king.tistory.com/308 /// dfs풀이
-// https://steady-coding.tistory.com/94 //플로이드 와샬 알고리즘 풀이
 import java.util.*;
+
 public class boj_11403 {
-    public static boolean[] visit;
-    public static int[][] ans;
-    public static ArrayList<Integer>[] graph;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
         int n = sc.nextInt();
-        ans = new int[n][n];
-        graph = new ArrayList[n];
-        
+        ArrayList<Integer>[] graph = new ArrayList[n];
+        boolean visit[];
         for (int i = 0; i < n; i ++) {
             graph[i] = new ArrayList<>();
-            for (int j = 0; j < n; j++) {
-                int num = sc.nextInt();
-                if (num == 1) {
+        }
+        for (int i = 0; i < n; i ++) {
+            for (int j = 0; j < n; j ++){
+                if (sc.nextInt() == 1) {
                     graph[i].add(j);
                 }
             }
         }
-        
-        // DFS 사용하여 해결하면 됨!
-        // ans를 순회
-        // graph가 1인 경우
-        for (int i = 0; i < n; i ++) {
-            visit = new boolean[n];
-            for (int j = 0; j < n; j++) {
-                
+        int[][]ans = new int[n][n];
+        for (int start = 0; start < n; start ++) {
+            for (int end = 0; end < n; end ++){
+                Queue<Integer> q = new LinkedList<>();
+                visit = new boolean[n];
+                q.add(start);
+                while(!q.isEmpty()){
+                    int k = q.poll();
+                    visit[k] = true;
+                    for (int tmp : graph[k]){
+                        if (tmp == end){
+                            ans[start][end] = 1;
+                            break;
+                        }
+                        if (!visit[tmp])
+                            q.add(tmp);
+                    }
+                    if (ans[start][end] == 1) break;
+                }
             }
         }
-        for (int i = 0; i < n; i ++) {
-            for (int j = 0; j < n; j++) {
+        System.out.println("------------");
+        for (int i = 0; i < n; i ++){
+            for (int j = 0; j < n;j ++){
                 System.out.print(ans[i][j] + " ");
             }
             System.out.println();
         }
+
     }
 }
