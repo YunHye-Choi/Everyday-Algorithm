1eee0b6217822c1bddc2e4861f88abd82e9685ae
anffl98@gmail.com <anffl98@gmail.com>
2022-09-13 20:31:13 +0900

    skip

---
+++ src/BOJ/Random_[solved.ac]/S1/boj_1629.java
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/1629
+// 풀이:: https://st-lab.tistory.com/237
+import java.util.*;
+public class boj_1629 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+        Long a = sc.nextLong();
+        Long b = sc.nextLong();
+        Long c = sc.nextLong();
+        // a를 b번 곱한 수를 c로 나눈 나머지
+
+        //skip.. 너무 뇌절임
+
+    }
+}
