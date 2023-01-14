da0d1e93fbedbd41fb6cf32900496ca634e58f7d
yunhye-choi <anffl98@gmail.com>
2023-01-14 23:53:35 +0900

    우아악

--- src/Programmers/Level2/크기가작은부분문자열.java
+++ src/Programmers/Level2/크기가작은부분문자열.java
@@ -1,6 +1,13 @@
 class Solution {
     public int solution(String t, String p) {
-        int answer = 0;
-        return answer;
-    }
+		int len = p.length();
+		long num = Long.parseLong(p);
+		int result = 0;
+
+		for (int i = 0; i < t.length() - len + 1; i++) {
+			long diff = Long.parseLong(t.substring(i, i + len));
+			if (diff <= num) result++;
+		}
+		return result;
+	}
 }
