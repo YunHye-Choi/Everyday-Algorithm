09c48a671c54141e7a79d7e44c4e864f7d468189
yunhye-choi <anffl98@gmail.com>
2023-01-28 22:57:02 +0900

    해결, 문자열 이용

--- src/leetcode/7._Reverse_Integer.java
+++ src/leetcode/7._Reverse_Integer.java
@@ -1,7 +1,5 @@
 class Solution {
     public int reverse(int x) {
-        
-        System.out.println("??");
         String str = String.valueOf(x);
         boolean isNeg = false;
         if (str.startsWith("-")) {
@@ -10,10 +8,9 @@ class Solution {
         }
         StringBuilder sb = new StringBuilder(str);
         sb.reverse();
-
-        int y = isNeg ? ((-1) * (int)(Long.parseLong(sb.toString()))) : (int)Long.parseLong(sb.toString());
-
-        if (y < (-1)*(Math.pow(2, 31)) && y > Math.pow(2, 31)-1) return 0;
-        return y;
+        long y = isNeg ? ((-1) * (Long.parseLong(sb.toString()))) : Long.parseLong(sb.toString());
+        if (y < (-1)*(Math.pow(2, 31)) || y > Math.pow(2, 31)-1) return 0;
+        int ans = (int)y;
+        return ans;
     }
 }
